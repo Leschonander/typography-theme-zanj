4a47312e3759b8120c2153d8cd48a5c7eb2bd104
Lars E. Schonander <33600597+Leschonander@users.noreply.github.com>
2018-05-04 22:56:04 -0400

    Creating new Gatsby Theme
    Basic Gatsby theme, will add some more features in a little bit, but excited to try open source!!!

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Typography from "typography";
+
+const typography = new Typography({
+    baseFontSize: '18px',
+    baseLineHeight: 1.45,
+    backgroundColor: '#f6f9fc',
+    bodyColor: '#333333',
+    googleFonts: [
+        {
+        name: 'Federo',
+        styles: ['400']
+        }
+    ],
+    headerFontFamily: ['Federo', 'san-serif'],
+    bodyFontFamily: ['Federo', 'san-serif'],
+  })
+  
+
+export default typography;
